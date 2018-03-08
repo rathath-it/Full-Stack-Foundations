fe4829c06976a424ebacf11e5fef9ee81258575d
abdennour <mail@abdennoor.com>
2018-03-08 06:06:32 +0300

    Make GET response more dynamic

--- Lesson-2/first-web-server/myserver.py
+++ Lesson-2/first-web-server/myserver.py
@@ -1,5 +1,7 @@
+# python 2.7
 import os
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from string import Template
 
 
 class MyHandler(BaseHTTPRequestHandler):
@@ -8,7 +10,7 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content/Type', 'application/json')
         self.end_headers()
-        self.wfile.write('{"message": "Welcome"}')
+        self.wfile.write(Template('{"message": "$path"}').safe_substitute({'path': self.path[1:]}))
         return
 
 
@@ -17,7 +19,7 @@ class MyHandler(BaseHTTPRequestHandler):
 def main():
     host = os.environ.get('HOST', '0.0.0.0')
     port = os.environ.get('PORT', 19999)
-    server = HTTPServer((host, port),MyHandler)
+    server = HTTPServer((host, port), MyHandler)
     server.serve_forever()
 
 if __name__ == '__main__':
